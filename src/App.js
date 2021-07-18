4b473eee6b1d029554dc934a6625e57da2eb1f57
Mukhammad <81352000+Muhammad0776@users.noreply.github.com>
2021-07-18 11:22:35 +0500

    TeamTimer

---
+++ src/App.js
@@ -0,0 +1,26 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './App.css';
+import Clock from './Components/Clock';
+import Loading from './Components/Loading';
+import LoadingWithHook from './Components/LoadingWithHook';
+import Pomodoro from './Components/Pomodoro';
+
+function App() {
+
+
+
+
+  return (
+    <div className="App text-center">
+      <Clock />
+      {/* <Loading /> */}
+
+      {/* Hook components */}
+      <LoadingWithHook />
+      <Pomodoro workTime={25} restTime={5} />
+      <Pomodoro workTime={30} restTime={10} />
+    </div>
+  );
+}
+
+export default App;
